d0301b4970fd12b18868a1f4311d835c0f96076c
Claijon da Silva <136808196+claijondasilvaaurivo@users.noreply.github.com>
2024-03-05 14:41:23 +0000

    changed to correct api endpoints

--- barcodescanner.py
+++ barcodescanner.py
@@ -130,7 +130,7 @@ def deviceIdentification():
     #print(res['error'])
     #print(res["data"])
     #print(res["data"]["data"])
-    print(res["data"]["data"][0]["jsondata"])
+    print(res["data"]["data"][0]["jsondata"]["bay"])
     if not res['error']:
         BAY = res["data"]["data"][0]["jsondata"]["bay"]
         LEDState('ready')
